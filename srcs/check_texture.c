efcc38bba0b04c17985c40c93b21c86d728b1da5
Reda TOUNSI <retounsi@e1r9p19.42.fr>
2019-12-03 12:12:16 +0100

    petite modif check_tecture

--- srcs/check_texture.c
+++ srcs/check_texture.c
@@ -6,7 +6,7 @@
 /*   By: retounsi <retounsi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/03 10:56:23 by retounsi          #+#    #+#             */
-/*   Updated: 2019/12/03 11:44:43 by retounsi         ###   ########.fr       */
+/*   Updated: 2019/12/03 12:11:59 by retounsi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ void	open_texture(t_env *e)
 {
 	int width;
 	int height;
-	if(!open(e->tex->north_texture, O_RDONLY))
+	if(!open(e->tex.north_texture, O_RDONLY))
 		ft_exit_params(e);
-	if(!open(e->tex->south_texture, O_RDONLY))
+	if(!open(e->tex.south_texture, O_RDONLY))
 		ft_exit_params(e);
-	if(!open(e->tex->east_texture, O_RDONLY))
+	if(!open(e->tex.east_texture, O_RDONLY))
 		ft_exit_params(e);
-	if(!open(e->tex->west_texture, O_RDONLY))
+	if(!open(e->tex.west_texture, O_RDONLY))
 		ft_exit_params(e);
-	if(!open(e->tex->sprite_texture, O_RDONLY))
+	if(!open(e->tex.sprite_texture, O_RDONLY))
 		ft_exit_params(e);
-	mlx_xpm_file_to_image(e->tex->north_img, e->tex->north_texture, &width, &height);
+	mlx_xpm_file_to_image(e->tex.north_img, e->tex.north_texture, &width, &height);
 }
