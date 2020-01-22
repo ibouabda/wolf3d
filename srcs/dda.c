e531cf11d316161689b24a5e31b04fdcaf7ad5f1
Idris BOUABDALLAH <idro30@hotmail.fr>
2020-01-22 16:06:37 +0100

    lol

--- srcs/dda.c
+++ srcs/dda.c
@@ -6,7 +6,7 @@
 /*   By: ibouabda <ibouabda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 16:36:51 by idris             #+#    #+#             */
-/*   Updated: 2020/01/21 17:36:10 by ibouabda         ###   ########.fr       */
+/*   Updated: 2020/01/22 15:58:56 by ibouabda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void ddax(t_env *e, double ystep)
     int x;
     int y;
 
-    x = (int)(e->playerx.x + 0.5);
-    y = (int)(e->playerx.y + 0.5);
+    x = (int)(e->playerx.x);
+    y = (int)(e->playerx.y);
     while (e->playerx.y >= 0 && e->playerx.x >= 0
     && e->playerx.y < e->mapy && e->playerx.y < e->mapx
 	&& x < e->mapx && y < e->mapy && e->dbtab[y][x] != '1')
@@ -35,8 +35,8 @@ void ddax(t_env *e, double ystep)
 			e->playerx.x--;
 			e->playerx.y = e->playerx.y + ystep;
 		}
-        x = (int)(e->playerx.x + 0.5);
-        y = (int)(e->playerx.y + 0.5);
+        x = (int)(e->playerx.x);
+        y = (int)(e->playerx.y);
     }
 	e->playerx.y = e->playerx.y - e->player.y;
 	e->playerx.x = e->playerx.x - e->player.x;
@@ -47,8 +47,8 @@ void dday(t_env *e, double xstep)
     int x;
     int y;
 
-    x = (int)(e->playery.x + 0.5);
-    y = (int)(e->playery.y + 0.5);
+    x = (int)(e->playery.x);
+    y = (int)(e->playery.y);
     while (e->playery.y >= 0 && e->playery.x >= 0
     && e->playery.y < e->mapy && e->playery.x < e->mapx
 	&& x < e->mapx && y < e->mapy && e->dbtab[y][x] != '1')
@@ -56,15 +56,15 @@ void dday(t_env *e, double xstep)
         if (e->rayang >= 0.0 && e->rayang <= 180.0)
 		{
         	e->playery.y--;
-			e->playery.x = e->playery.x - xstep;
+			e->playery.x = e->playery.x + xstep;
 		}
 		else
 		{
 			e->playery.y++;
-        	e->playery.x = e->playery.x + xstep;
+        	e->playery.x = e->playery.x - xstep;
 		}
-        x = (int)(e->playery.x + 0.5);
-        y = (int)(e->playery.y + 0.5);
+        x = (int)(e->playery.x);
+        y = (int)(e->playery.y);
     }
 	e->playery.y = e->playery.y - e->player.y;
 	e->playery.x = e->playery.x - e->player.x;
@@ -77,23 +77,43 @@ void		ft_dda(t_env *e)
 
 	printf("rayang = %f\n", e->rayang);
     ystep = tan(e->rayang / e->pi);
-    xstep = 1 / ystep;
+    xstep = tan((90 - e->rayang) / e->pi);
     printf("xstep = %f, ystep = %f\n", xstep, ystep);
-    e->playerx.x = 1.0 - (e->player.x - ((int)(e->player.x)));
-    e->playerx.y = e->player.y + ystep * e->playerx.x;
-    e->playerx.x = e->playerx.x + e->player.x;
+	if ((e->rayang >= 270.0 && e->rayang <= 360.0)
+    || (e->rayang >= 0.0 && e->rayang <= 90.0))
+	{
+		e->playerx.x = 1.0 - (e->player.x - ((int)(e->player.x)));
+		e->playerx.y = e->player.y - ystep * e->playerx.x;
+		e->playerx.x = e->playerx.x + e->player.x;
+	}
+	else
+	{
+		e->playerx.x = (e->player.x - ((int)(e->player.x)));
+		e->playerx.y = e->player.y + ystep * e->playerx.x;
+		e->playerx.x = e->player.x - e->playerx.x;
+	}
 	// e->playerx.x = e->player.x;
 	// e->playerx.y = e->player.y;
 	// e->playery.x = e->player.x;
 	// e->playery.y = e->player.y;
-    ddax(e, ystep);
 	printf("e->playerx.x = %f, e->playerx.y = %f\n", e->playerx.x, e->playerx.y);
+    ddax(e, ystep);
+
     // printf("ddax\n");
-    e->playery.y = (e->player.y - ((int)(e->player.y)));
-    e->playery.x = e->player.x + xstep * e->playery.y;
-    e->playery.y = e->player.y - e->playery.y;
-    dday(e, xstep);
+	if (e->rayang >= 0.0 && e->rayang <= 180.0)
+	{
+    	e->playery.y = (e->player.y - ((int)(e->player.y)));
+    	e->playery.x = e->player.x + xstep * e->playery.y;
+    	e->playery.y = e->player.y - e->playery.y;
+	}
+	else
+	{
+		e->playery.y = 1.0 - (e->player.y - ((int)(e->player.y)));
+    	e->playery.x = e->player.x - xstep * e->playery.y;
+    	e->playery.y = e->player.y + e->playery.y;
+	}
 	printf("e->playery.x = %f, e->playery.y = %f\n", e->playery.x, e->playery.y);
+    dday(e, xstep);
     // printf("dday\n");
     e->playery.y = e->playery.y * e->playery.y;
     e->playery.x = e->playery.x * e->playery.x;
